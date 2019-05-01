4144f74e21cefb7d803e891415288b4462a8c208
Kenta Aikawa <b1015110@fun.ac.jp>
2019-05-01 15:03:29 +0900

    Initial Commit

---
+++ Spock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spock
+//
+//  Created by 相川健太 on 2019/05/01.
+//  Copyright © 2019 相川健太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
