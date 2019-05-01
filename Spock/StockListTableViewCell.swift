4e1bd0e2311e85dfdd37bd32f5f3b0b9ac6ba832
Kenta Aikawa <b1015110@fun.ac.jp>
2019-05-01 16:05:05 +0900

    first commit

---
+++ Spock/StockListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StockListTableViewCell.swift
+//  Spock
+//
+//  Created by 相川健太 on 2019/05/01.
+//  Copyright © 2019 相川健太. All rights reserved.
+//
+
+import UIKit
+
+class StockListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
